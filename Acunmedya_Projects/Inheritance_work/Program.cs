dac2cd95997277328d9b20765c1e8f47c0de12c3
Emre Taştanberk <24301071009@kayseri.edu.tr>
2025-02-28 20:11:13 +0300

    Completedd

---
+++ Acunmedya_Projects/Inheritance_work/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+
+
+namespace Inheritance_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            Mercedes mercedes = new Mercedes();
+            mercedes.GetCarInfos("Mercedes", "Manual");
+
+            Porsche porsche = new Porsche();
+            porsche.GetCarInfos("Porche", "Automatic");
+
+            Togg togg = new Togg();
+            togg.GetCarInfos("Togg", "automatic");
+
+            Bmw bmw = new Bmw();
+            bmw.GetCarInfos("Bmw","Manual");
+
+            Audi audi = new Audi();
+            audi.GetCarInfos("Audi", "automatic");
+
+            Toyota toyota = new Toyota();
+            toyota.GetCarInfos("Toyota", "Manual");
+
+            
+
+        }
+
+        public class Car
+        {
+            
+            public void GetCarInfos(string brand, string transmission)
+            {
+                Console.WriteLine($"{brand} ---> {transmission}");
+            }
+
+
+        }
+
+        public class Mercedes : Car
+        {
+
+        }
+
+        public class Porsche : Car
+        {
+
+        }
+
+        public class Togg : Car
+        {
+
+        }
+
+        public class Toyota : Car
+        {
+
+        }
+
+        public class Audi : Car
+        {
+
+        }
+
+        public class Bmw : Car
+        {
+
+        }
+    }
+}

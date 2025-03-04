76f2934ee972ae51f5c43dec4dd3bca364742e06
Emre Taştanberk <24301071009@kayseri.edu.tr>
2025-03-04 20:05:42 +0300

    _04.03

---
+++ Acunmedya_Projects/Access_Modifiers/Classes/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers.Classes
+{
+    internal class User
+    {
+
+        private string nameSurname;
+        private int age;
+        public string eMail;
+
+        public string NameSurname
+        {
+            get { return nameSurname;}
+            set { nameSurname = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public void showInfos()
+        {
+            Console.WriteLine($"Name and surname: {nameSurname} Age:{age}");
+        }
+    }
+}
---
+++ Acunmedya_Projects/Access_Modifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection.Metadata;
+using Access_Modifiers.Classes;
+
+
+namespace Access_Modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.eMail = "emretastanberk@gmail.com";
+            user.NameSurname = "Emre Taştanberk";
+            user.Age = 18;
+            user.showInfos();
+        }
+    }
+}
+
